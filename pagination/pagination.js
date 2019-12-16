cf9f85e4b0f3e63ef23d8558b0dc2cb6c215eca0
Fred <33107804+Fredxingxing@users.noreply.github.com>
2019-12-16 17:52:10 +0800

    fix pagination

--- pagination/pagination.js
+++ pagination/pagination.js
@@ -1,76 +1,93 @@
-var pagination = (function () {
-  var $paginationDom = null
-  var page = 1;
-  var defualt = {
+function Pagination (option) {
+  this.$paginationDom = null
+  this.defualt = {
+    page:1,
     total_pages:3,
-    id:'#pagination',
+    id:'pagination',
     color:'',
-    nextPageClick:function() {
+    pageClick:function() {
 
     },
-    lastPageClick:function( ) {
-    }
   }
+  this.defualt = $.extend(this.defualt,option)
+  this.$paginationDom = $('#'+this.defualt.id)
+  console.log(this.defualt)
   //页脚
-  function render (option) {
-    defualt = $.extend(defualt,option)
-    $paginationDom = $(defualt.id)
-    $paginationDom.html('')
+  this.render =  function () {
+    this.$paginationDom.html('')
+    var _this = this
     var $lastDom = '<li class="page-item" id="lastPage"><span class="page-link" >上一页</span></li>'
     var $nextDom = '<li class="page-item" id="nextPage"><span class="page-link" >下一页</span></li>'
-    if (!defualt || defualt.total_pages == 1) {
+    if (!this.defualt || this.defualt.total_pages == 1) {
       $lastDom = ''
       $nextDom = ''
     }
-    $paginationDom.append($lastDom)
+    this.$paginationDom.append($lastDom)
     $('#lastPage').click(function () {
-      lastPageClick()
+      _this.lastPageClick()
     })
     // 要判断剩余页少于6的时候
-    var page_max = page + 5 > defualt.total_pages ? defualt.total_pages : page + 5
+    var page_max = this.defualt.page + 5 > this.defualt.total_pages ? this.defualt.total_pages : this.defualt.page + 5
     var page_min = 0;
-    if(defualt.total_pages>6){
-      page_min = page - 1 < defualt.total_pages - 6 ? page - 1 : defualt.total_pages - 6
+    if(this.defualt.total_pages>6){
+      page_min = this.defualt.page - 1 < this.defualt.total_pages - 6 ? this.defualt.page - 1 : this.defualt.total_pages - 6
     }
+    console.log(111)
     for (var index = page_min; index < page_max; index++) {
-      var activeClass = page === (index + 1) ? 'page-active' : ''
-      var $pageDom = '<li class="page-item" id="' + ('page-' + (index + 1)) + '"><span class="page-link ' + activeClass + '" >' + (index + 1) + '</span></li>'
-      $paginationDom.append($pageDom);
+      var activeClass = this.defualt.page === (index + 1) ? 'page-active' : ''
+      var $pageDom = '<li class="page-item" id="' + (this.defualt.id+'-' + (index + 1)) + '">' +
+        '<span class="page-link ' + activeClass + '" >' + (index + 1) + '</span>' +
+        '</li>'
+      this.$paginationDom.append($pageDom);
       (function (index) {
-        $('#' + 'page-' + (index + 1)).click(function () {
-          pageClick(index + 1)
+        console.log('index////',index)
+        $('#' + _this.defualt.id +'-' + (index + 1)).click(function () {
+          console.log("click---",index)
+          _this.pageClick(index + 1)
         })
       })(index)
     }
-    if (defualt.total_pages > 6 && (defualt.total_pages - page) > 5) {
+    console.log(2222)
+    if (this.defualt.total_pages > 6 && (this.defualt.total_pages - this.defualt.page) > 5) {
       var $moreDom = '<li class="page-item"><span class="page-link" >...</span></li>'
-      $paginationDom.append($moreDom)
+      this.$paginationDom.append($moreDom)
     }
-    $paginationDom.append($nextDom)
+    this.$paginationDom.append($nextDom)
     $('#nextPage').click(function () {
-      nextPageClick(defualt.total_pages)
+      _this.nextPageClick(_this.defualt.total_pages)
     })
   }
 
 //页脚点击
-  function pageClick (pageNum) {
-    page = pageNum
-    searchActivityStyle(pageNum)
+  this.pageClick = function (pageNum) {
+    console.log(this.defualt.page,pageNum)
+    if(this.defualt.page===pageNum){
+      return
+    }
+    this.defualt.page = pageNum
+    this.defualt.pageClick(pageNum)
+    // this.render()
     window.scrollTo(0, 0)
   }
 
-  function lastPageClick () {
-    page > 1 ? page-- : page
-    searchActivityStyle(page)
+  this.lastPageClick = function () {
+    if(this.defualt.page < 2){
+      return
+    }
+    this.defualt.page--
+    this.defualt.pageClick(this.defualt.page)
+    this.render()
     window.scrollTo(0, 0)
   }
 
-  function nextPageClick (total_pages) {
-    page < total_pages ? page++ : total_pages
-    searchActivityStyle(page)
+  this.nextPageClick = function (total_pages) {
+    if(this.defualt.page < total_pages){
+      this.defualt.page++
+    }else{
+      return
+    }
+    this.defualt.pageClick(this.defualt.page)
+    this.render()
     window.scrollTo(0, 0)
   }
-  return{
-    render:render
-  }
-})()
+}
